91ef2f7b4a57bfa44cf0bcf8dbcea2a5c5129e0f
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-08-03 14:40:18 -0300

    Update svm_t.R

--- models/svm_t/svm_t.R
+++ models/svm_t/svm_t.R
@@ -1,20 +1,22 @@
 ################################################################################
 #### librarys
-library( ggplot2 )
-library(patchwork)
-# getwd()
-path = 'C:/Users/8936381/Documents/Simulacao/Estudos_Simulacao/ts/ts_model.cpp'
-Rcpp::sourceCpp( path )
-
+#library( ggplot2 )
+#library(patchwork)
+source( 'https://raw.githubusercontent.com/holtz27/svmsmn/main/source/num_analisys.R' )
+source( 'https://raw.githubusercontent.com/holtz27/svmsmn/main/source/figures.R' )
 source('https://raw.githubusercontent.com/holtz27/svmsmn/main/source/data/t_data.R')
+source( 'https://raw.githubusercontent.com/holtz27/svmsmn/main/source/model.selection.R' )
 
-y = ts_data(mu = -1, phi = 0.95, sigma = 0.15,
-            b0 = 0.1, b1 = 0.01, b2 = -0.05,
-            y0 = 0,
-            v = 20,
-            T = 2e3,
-            seed = 42)$y
+y = t_data(mu = -1, phi = 0.95, sigma = 0.15,
+           b0 = 0.1, b1 = 0.01, b2 = -0.05,
+           y0 = 0,
+           v = 20,
+           T = 2e3,
+           seed = 42)$y
 
+# getwd()
+path = 'C:/Users/8936381/Documents/svm_smn/Simulacao/Estudos_Simulacao/ts/ts_model.cpp'
+Rcpp::sourceCpp( path )
 # Sampling
 # 67092,  N = 5e3: cadeia corre
 # 439675, N = 5e3
@@ -25,19 +27,19 @@ y = ts_data(mu = -1, phi = 0.95, sigma = 0.15,
 # phi: L * eps = 15
 # sigma: L * eps = 3
 
-N = 5e4
-samples = svm_smn_ts(N,
-                     L_theta = 20, eps_theta = c( 0.5, 0.5, 0.5 ), 
-                     L_b = 20, eps_b = c( 0.1, 0.1, 0.1 ), 
-                     L_h = 50, eps_h = 0.015,
-                     L_v = 20, eps_v = 0.05, 
-                     y_T = c(0, y), 
-                     seed = 67092 )
+N = 5e3
+samples = svm_t(N,
+                L_theta = 20, eps_theta = c( 0.5, 0.5, 0.5 ), 
+                L_b = 20, eps_b = c( 0.1, 0.1, 0.1 ), 
+                L_h = 50, eps_h = 0.01,
+                L_v = 1, eps_v = 0.01, 
+                y_T = c(0, y), 
+                seed = 0 )
 samples$time / 60
 samples$acc / N
 ################## Save outputs
 #save(samples, file = 'C:/Users/8936381/Documents/Simulacao/Estudos_Simulacao/ts/ts_ES.RDara')
-load('C:/Users/8936381/Documents/Simulacao/Estudos_Simulacao/ts/ts_ES.RDara')
+#load('C:/Users/8936381/Documents/Simulacao/Estudos_Simulacao/ts/ts_ES.RDara')
 chain_theta = samples$chain$chain_theta
 chain_b = samples$chain$chain_b
 chain_h = samples$chain$chain_h
@@ -47,159 +49,67 @@ chain_l = samples$chain$chain_l
 chain_theta[2, ] = tanh( chain_theta[2, ] )
 chain_theta[3, ] = exp( chain_theta[3, ] )
 chain_b[2, ]     = tanh( chain_b[2, ] )
-chain_v          = exp( chain_e[1, ] )
-N = dim(chain_theta)[2] - 1
+# v = 0.5 * ( (ls - li) * tanh( 0.5 * alpha * e ) + (ls + li) )
+chain_v          =  0.5 * ( (40 - 2) * tanh( 0.5 * 1 * chain_e[1, ] ) + (40 + 2) ) 
+
+# Draws
+draws = matrix(c( chain_theta[1, ],
+                  chain_theta[2, ],
+                  chain_theta[3, ],
+                  chain_b[1, ],
+                  chain_b[2, ],
+                  chain_b[3, ],
+                  chain_v
+                  ), nrow = 7, byrow = TRUE)
+draws = rbind( draws, chain_h )
+draws = rbind( draws, chain_l )
 ############################### Convergence analysis
 ################### Trace plots
 ### burn
-burn = 2e4
-chain_theta  = chain_theta[, - c( 1:burn ) ] 
-chain_b     = chain_b[, - c( 1:burn ) ]
-chain_h     = chain_h[, - c( 1:burn ) ]
-chain_e     = chain_e[ - c( 1:burn ) ]
-chain_v     = chain_v[ - c( 1:burn ) ]
-chain_l     = chain_l[, - c( 1:burn ) ] 
-# Jumps
-lags = 20
+burn = 2e3
+draws = draws[, -c( 1:burn )]
+lags = 10
 jumps = seq(1, N - burn, by = lags)
-chain_theta = chain_theta[, jumps ]
-chain_b     = chain_b[, jumps ]
-chain_h     = chain_h[, jumps ]
-chain_e     = chain_e[ jumps ]
-chain_v     = chain_v[ jumps ]
-chain_l     = chain_l[, jumps ]
-N_new = length( jumps )
-###############################################################################
-###############################################################################
-############################## Análise gráfica
-mat = matrix(seq(1, 21), nrow = 3, ncol = 7)
-layout( mat )
-############################### theta
-plot(chain_theta[1, ], type = 'l', main = 'mu', xlab = '', ylab = '')
-plot(acf(chain_theta[1, ], lag.max = 100, plot = FALSE)[1:100], main = '', 
-     xlab = '', ylab = 'ACF')
-plot(density(chain_theta[1, ]), main = '', xlab = '', ylab = '' )
-plot(chain_theta[2, ], type = 'l', main = 'phi', xlab = '', ylab = '')
-plot(acf(chain_theta[2, ], lag.max = 100, plot = FALSE)[1:100], main = '', 
-     xlab = '', ylab = 'ACF')
-plot(density(chain_theta[2, ]), main = '', xlab = '', ylab = '' )
-plot(chain_theta[3, ], type = 'l',  main = 'sigma', xlab = '', ylab = '')
-plot(acf(chain_theta[3, ], lag.max = 100, plot = FALSE)[1:100], main = '', 
-     xlab = '', ylab = 'ACF')
-plot(density(chain_theta[3, ]), main = '', xlab = '', ylab = '' )
-############################### b
-plot(chain_b[1, ], type = 'l', main = 'b0', xlab = '', ylab = '')
-plot(acf(chain_b[1, ], lag.max = 100, plot = FALSE)[1:100], main = '', 
-     xlab = '', ylab = 'ACF')
-plot(density(chain_b[1, ]), main = '', xlab = '', ylab = '' )
-plot(chain_b[2, ], type = 'l', main = 'b1', xlab = '', ylab = '')
-plot(acf(chain_b[2, ], lag.max = 100, plot = FALSE)[1:100], main = '', 
-     xlab = '', ylab = 'ACF')
-plot(density(chain_b[2, ]), main = '', xlab = '', ylab = '' )
-plot(chain_b[3, ], type = 'l', main = 'b2', xlab = '', ylab = '')
-plot(acf(chain_b[3, ], lag.max = 100, plot = FALSE)[1:100], main = '', 
-     xlab = '', ylab = 'ACF')
-plot(density(chain_b[3, ]), main = '', xlab = '', ylab = '' )
-############################### e = log( v )
-plot(chain_e, type = 'l', main = 'e = log( v )', xlab = '', ylab = '')
-plot(acf(chain_e, lag.max = 100, plot = FALSE)[1:100], main = '', 
-     xlab = '', ylab = '')
-plot(density(chain_e), main = '', xlab = '', ylab = '' )
-############### Numeric Analysis
-############################### theta
-mcmcchain_theta = coda::as.mcmc( t( chain_theta ) )
-####### Geweke Statistic
-# |G| > 1.96 evidencia não convergencia
-CD_theta = coda::geweke.diag( mcmcchain_theta )
-####### Fator de ineficiência (IF)
-# IF = N / N_eff, onde N_eff = effective Sample Size
-# Se IF >> 1, indica uma má mistura da cadeia gerada
-N_eff_theta = coda::effectiveSize( mcmcchain_theta )
-IF_theta = N_new / N_eff_theta
-####### MCMC error
-# MCerror = sd( Variavel ) / sqrt( N_eff )
-mc_error_theta = apply( chain_theta, 
-                        MARGIN = 1, 
-                        FUN = sd) / sqrt( N_eff_theta )
-theta_hat = apply( chain_theta, MARGIN = 1, FUN = mean )
-theta_sd = apply( chain_theta, MARGIN = 1, FUN = sd )
-theta_min = apply( chain_theta, MARGIN = 1, FUN = quantile, probs = c(0.025) )
-theta_max = apply( chain_theta, MARGIN = 1, FUN = quantile, probs = c(0.975) )
-data_theta = matrix(
-                    c(theta_hat,
-                      theta_sd,
-                      theta_min,
-                      theta_max,
-                      CD_theta$z,
-                      IF_theta,
-                      mc_error_theta), nrow = 3, byrow = FALSE
-                    )
-row.names( data_theta ) = c('mu', 'phi', 'sigma')
-############################### b
-mcmcchain_b = coda::as.mcmc( t( chain_b ) )
-####### Geweke Statistic
-# |G| > 1.96 evidencia não convergencia
-CD_b = coda::geweke.diag( mcmcchain_b )
-####### Fator de ineficiência (IF)
-# IF = N / N_eff, onde N_eff = effective Sample Size
-# Se IF >> 1, indica uma má mistura da cadeia gerada
-N_eff_b = coda::effectiveSize( mcmcchain_b )
-IF_b = N_new / N_eff_b
-####### MCMC error
-# MCerror = sd( Variavel ) / sqrt( N_eff )
-mc_error_b = apply( chain_b, 
-                    MARGIN = 1, 
-                    FUN = sd) / sqrt( N_eff_b )
-b_hat = apply( chain_b, MARGIN = 1, FUN = mean )
-b_sd = apply( chain_b, MARGIN = 1, FUN = sd )
-b_min = apply( chain_b, MARGIN = 1, FUN = quantile, probs = c(0.025) )
-b_max = apply( chain_b, MARGIN = 1, FUN = quantile, probs = c(0.975) )
-data_b = matrix(
-                c(b_hat,
-                  b_sd,
-                  b_min,
-                  b_max,
-                  CD_b$z,
-                  IF_b,
-                  mc_error_b), nrow = 3, byrow = FALSE
-               )
-row.names( data_b ) = c('b0', 'b1', 'b2')
-############################### e = log( v )
-mcmcchain_e = coda::as.mcmc( chain_e ) 
-####### Geweke Statistic
-# |G| > 1.96 evidencia não convergencia
-CD_e = coda::geweke.diag( mcmcchain_e )
-####### Fator de ineficiência (IF)
-# IF = N / N_eff, onde N_eff = effective Sample Size
-# Se IF >> 1, indica uma má mistura da cadeia gerada
-N_eff_e = coda::effectiveSize( mcmcchain_e )
-IF_e = N_new / N_eff_e
-####### MCMC error
-# MCerror = sd( Variavel ) / sqrt( N_eff )
-mc_error_e = sd( chain_e ) / sqrt( N_eff_e )
-e_hat = mean( chain_e )
-e_sd = sd( chain_e )
-e_min = quantile( chain_e, probs = c(0.025) )
-e_max = quantile( chain_e, probs = c(0.975) )
-data_e = matrix(
-                c(e_hat,
-                  e_sd, 
-                e_min,
-                e_max,
-                CD_e$z,
-                IF_e,
-                mc_error_e), nrow = 1, byrow = FALSE
-               )
-row.names( data_e ) = c('e')
-###############################################################################
-###############################################################################
-# Summary Table
-data = data_theta
-data = rbind( data, data_b, data_e )
-#data = cbind( c(mu, phi, sigma, b0, b1, b2, log(v) ), data )
-colnames( data ) = c('média', 'sd', '2.5%', '97.5%', 'CD', 'IF', 'mc_error')
-data = round( data, 4 )
-data
+draws = draws[, jumps ]
+
+trace_plots(draws[1:7, ], 
+            names = c('mu', 'phi', 'sigma', 'b0', 'b1', 'b2', 'v') )
+################### Numeric Analysis
+num_analisys(draws[1:7, ], 
+             names = c( 'mu', 'phi', 'sigma', 'b0', 'b1', 'b2', 'v'),
+             digits = 4 )
+################################################################################
+############################## Model Selection
+# p( y | theta ) = p( y | b, theta_h, v, h ) = p( y | b, h )
+# data = ( y0 , y )
+# param_hat = ( b = b_hat, h = h_hat, l = l_hat )
+################################################################################
+############### dic
+ts_dic = svmsmn_dic(data = y, y0 = 0, 
+                    param_draws = draws[ c( 4:6, 
+                                            8:(T+7),
+                                            (T+8):(nrow(draws)) ), ]
+)
+############### waic
+ts_waic = svmsmn_waic(data = y, y0 = 0,
+                      draws = draws[ c( 4:6, 
+                                        8:(T+7),
+                                        (T+8):(nrow(draws)) ), ]
+)
+############### loo
+ts_loo = svmsmn_loo(data = y, y0 = 0, 
+                    draws = draws[ c( 4:6, 
+                                      8:(T+7),
+                                      (T+8):(nrow(draws)) ), ],
+                    cores = 4
+)
+ts_criterium = matrix( c(ts_dic, 
+                         ts_loo$estimates[3,1],
+                         ts_waic$estimates[3,1]), nrow = 1)
+row.names( ts_criterium ) = c('ts')
+colnames( ts_criterium ) = c('dic', 'loo', 'waic')
+ts_criterium
+
 ###############################################################################
 ###############################################################################
 ############################### l
@@ -313,151 +223,3 @@ plot( IF_h, main = 'Inefficiency factors', xlab = '', ylab = '' )
 abline(h = 1)
 plot( mc_error_h, main = 'MCMC errors', xlab = '', ylab = '' )
 par( mfrow = c(1,1) )
-###############################################################################
-###############################################################################
-############################## Model Selection
-# construindo theta_hat e theta_draws
-theta_hat = c( b_hat, h_hat, l_hat )
-theta_draws = chain_b
-theta_draws = rbind( theta_draws, chain_h )
-theta_draws = rbind( theta_draws, chain_l )
-############### dic deviance information criterion:
-# p( y | theta ) = p( y | b, theta_h, v, h, l) = p( y | b, h, l )
-# theta = b, h, l
-# data = c( y0, y )
-# theta_hat = ( b_hat, h_hat, l_hat )
-# theta_draws = burned_lag
-log_lik = function(theta_t, data){
-  # função checada (13/04/23)
-  T = length( data ) - 1
-  
-  b0_t = theta_t[1]
-  b1_t = theta_t[2]
-  b2_t = theta_t[3]
-  h_t = theta_t[4:(T+3)]
-  l_t = theta_t[(T + 4):(2 * T + 3)]
-  log_l = dnorm(data[2:( T + 1 )], 
-                mean = b0 + b1 * data[1:T] + b2 * exp( h_t ), 
-                sd = exp( 0.5 * h_t ) / sqrt( l_t ), 
-                log = TRUE)
-  
-  return( sum( log_l ) )
-  
-}
-log_lik_i = function(data, theta_draws){
-  # função checada (13/04/23)
-  x = apply(X = theta_draws, MARGIN = 2, FUN = log_lik, data)
-  
-  return( x )
-}
-dic = function(data, theta_draws, theta_hat){
-  
-  pD = 2 * ( log_lik( theta_hat, data ) - mean( log_lik_i(data, theta_draws) ) )      
-  DIC = - 2 * log_lik( theta_hat, data ) + 2 * pD  
-  
-  return( DIC )
-} 
-# calculando DIC
-dic_ts = dic( data = c(y0, y), 
-              theta_draws = theta_draws, 
-              theta_hat )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-############### loo
-lik = function(data_i, draws, data_, data_past){
-  #data_ = ( y_{1}, y_{2}, ..., y_{T} )
-  #data_past = ( y_{0}, y_{1}, ..., y_{T-1} )
-  k = which( data_ == as.numeric( data_i ) )
-  log_l = NULL
-  N = ncol( draws )
-  T = 0.5 * (nrow( draws ) - 3 )
-  for(col in 1:N){
-    
-    b0_draws = draws[1, col]
-    b1_draws = draws[2, col]
-    b2_draws = draws[3, col]
-    h_draws  = draws[3 + k, col]
-    l_draws  = draws[T + 3 + k, col] 
-    
-    log_l[col] = dnorm(data_i, mean = b0_draws + b1_draws * data_past[k] + b2_draws * exp( h_draws ), 
-                       sd = exp( 0.5 * h_draws ) / sqrt( l_draws ) )
-    
-  }
-  
-  return( log_l )
-}
-
-r_eff = loo::relative_eff(lik,
-                          chain_id = rep(1, ncol( theta_draws ) ),
-                          data = as.matrix( y ), 
-                          draws = theta_draws,
-                          data_ = y,
-                          data_past = c( y0, y[1:(T-1)] )
-                          #cores = getOption('mc.cores', 3)
-)
-
-# or set r_eff = NA
-loo_ts = loo::loo(lik, 
-                  #r_eff = NA,
-                  r_eff = r_eff, 
-                  data = as.matrix( y ), 
-                  draws = theta_draws,
-                  data_ = y,
-                  data_past = c( y0, y[1:(T-1)] ),
-                  cores = getOption('mc.cores', 3)
-)
-
-############### waic
-log_lik = function(data_i, draws, data_, data_past){
-  #data_ = ( y_{1}, y_{2}, ..., y_{T} )
-  #data_past = ( y_{0}, y_{1}, ..., y_{T-1} )
-  k = which( data_ == as.numeric( data_i ) )
-  log_l = NULL
-  N = ncol( draws )
-  T = 0.5 * (nrow( draws ) - 3 )
-  for(col in 1:N){
-    
-    b0_draws = draws[1, col]
-    b1_draws = draws[2, col]
-    b2_draws = draws[3, col]
-    h_draws  = draws[3 + k, col]
-    l_draws  = draws[T + 3 + k, col] 
-    
-    log_l[col] = dnorm(data_i, mean = b0_draws + b1_draws * data_past[k] + b2_draws * exp( h_draws ), 
-                       sd = exp( 0.5 * h_draws ) / sqrt( l_draws ), log = TRUE )
-    
-  }
-  
-  return( log_l )
-}
-
-waic = loo::waic(log_lik, 
-                 data = matrix( y, ncol = 1 ), 
-                 draws = theta_draws,
-                 data_ = y,
-                 data_past = c( y0, y[1:(T-1)] )
-)
-
-ts_criterium = matrix( c(dic_ts, 
-                         loo_ts$looic,
-                         waic$waic), nrow = 1)
-row.names( ts_criterium ) = c('ts')
-colnames( ts_criterium ) = c('dic', 'loo', 'waic')
-ts_criterium
-
