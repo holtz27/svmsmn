712ba571578df50079c6c905314d26e772df7d91
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-08-15 17:30:09 -0300

    Update ibovespa_n.R

--- aplicação/ibovespa_n.R
+++ aplicação/ibovespa_n.R
@@ -1,4 +1,3 @@
-
 # Reading data
 ibovespa = read.csv('https://raw.githubusercontent.com/holtz27/rbras/main/aplica%C3%A7%C3%B5es/%5EBVSP.csv')
 ibovespa = ibovespa[, c('Date', 'Close')]
@@ -7,7 +6,8 @@ ibovespa = na.omit(ibovespa)
 ibovespa = tail(ibovespa, n = 3992)
 #View(ibovespa)
 T = nrow(ibovespa)
-log.ret = 100 * ( log( ibovespa[2:T, 2] ) - log( ibovespa[1:(T-1), 2] ) ) 
+log.ret = 100 * ( log( ibovespa[2:T, 2] ) - log( ibovespa[1:(T-1), 2] ) )
+dates = as.Date( ibovespa[, 1] )
 ################################################################################
 #### librarys
 #### librarys
@@ -25,7 +25,7 @@ samples = svmn(N,
                L_b = 20, eps_b = 0.1,
                L_h = 50, eps_h = 0.015,
                y_T = c(0, log.ret),
-               seed = 976335 ) 
+               seed = 0 ) 
 
 samples$time / 60
 samples$acc / N
@@ -44,6 +44,7 @@ draws = matrix(c( chain_theta[1, ],
                   chain_b[3, ]
                   ), nrow = 6, byrow = TRUE )
 draws = rbind( draws, chain_h )
+draws = rbind( draws, matrix(1, nrow = T, ncol = N + 1) )
 ############################### Convergence analysis
 ################### Trace plots
 ### burn
@@ -55,7 +56,23 @@ draws = draws[, jumps ]
 # plots
 trace_plots(draws[1:6, ], 
             names = c('mu', 'phi', 'sigma', 'b0', 'b1', 'b2') )
+abs_plots(draws[7:nrow(draws), ], dates[-1], log.ret)
 ################### Numeric Analysis
 num_analisys(draws[1:6, ], 
              names = c( 'mu', 'phi', 'sigma', 'b0', 'b1', 'b2'),
              digits = 4 )
+###############################################################################
+############################## Model Selection
+# p( y | theta ) = p( y | b, theta_h, v, h ) = p( y | b, h )
+# data = ( y0 , y )
+# param_hat = ( b = b_hat, h = h_hat, l = l_hat )
+###############################################################################
+############### DIC
+normal_dic = svmsmn_dic(data = log.ret, y0 = 0, 
+                        param_draws = draws[4:(nrow(draws)), ])
+############### waic
+normal_waic = svmsmn_waic(data = log.ret, y0 = 0,
+                          draws = draws[4:(nrow(draws)), ])
+############### loo
+normal_loo = svmsmn_loo(data = log.ret, y0 = 0, 
+                        draws = draws[4:(nrow(draws)), ])
