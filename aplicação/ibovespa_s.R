05e3f1b7f3ea3ed15f0968323ad0c910e0f16534
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-08-16 14:12:33 -0300

    Update ibovespa_s.R

--- aplicação/ibovespa_s.R
+++ aplicação/ibovespa_s.R
@@ -16,11 +16,11 @@ source( 'https://raw.githubusercontent.com/holtz27/svmsmn/main/source/figures.R'
 source( 'https://raw.githubusercontent.com/holtz27/svmsmn/main/source/model.selection.R' )
 
 # getwd()
-path = 'svm_smn/Simulacao/Estudos_Simulacao/slash/svm_s.cpp'
+path = 'svm_s/svm_s.cpp'
 Rcpp::sourceCpp( path )
 
 # Sampling
-N = 5e3
+N = 5e4
 samples = svm_s(N,
                 L_theta = 20, eps_theta = c( 0.5, 0.5, 0.5 ), 
                 L_b = 20, eps_b = c( 0.1, 0.1, 0.1 ), 
@@ -51,14 +51,15 @@ draws = rbind( draws, chain_l )
 ############################### Convergence analysis
 ################### Trace plots
 ### burn
-burn = 0
+burn = 2e4
 burned_draws = draws[, -c( 1:burn )]
-lags = 1
+lags = 20
 jumps = seq(1, N - burn, by = lags)
 laged_draws = burned_draws[, jumps ]
 trace_plots(laged_draws[1:7, ], 
             names = c('mu', 'phi', 'sigma', 'b0', 'b1', 'b2', 'v') )
 abs_plots(chain_h, dates[-1], log.ret)
+tail_plot(chain_l, dates[-1], 'S')
 ################### Numeric Analysis
 num_analisys(laged_draws[1:7, ], 
              names = c( 'mu', 'phi', 'sigma', 'b0', 'b1', 'b2', 'v'),
@@ -70,28 +71,27 @@ num_analisys(laged_draws[1:7, ],
 # param_hat = ( b = b_hat, h = h_hat, l = l_hat )
 ################################################################################
 ############### dic
-ts_dic = svmsmn_dic(data = y, y0 = 0, 
-                    param_draws = draws[ c( 4:6, 
-                                            8:(T+7),
-                                            (T+8):(nrow(draws)) ), ]
+slash_dic = svmsmn_dic(data = log.ret, y0 = 0, 
+                       param_draws = draws[ c( 4:6, 
+                                               8:(T+7),
+                                               (T+8):(nrow(draws)) ), ]
 )
 ############### waic
-slash_waic = svmsmn_waic(data = y, y0 = 0,
+slash_waic = svmsmn_waic(data = log.ret, y0 = 0,
                          draws = draws[ c( 4:6, 
-                                        8:(T+7),
-                                        (T+8):(nrow(draws)) ), ]
+                                           8:(T+7),
+                                           (T+8):(nrow(draws)) ), ]
 )
 ############### loo
-slash_loo = svmsmn_loo(data = y, y0 = 0, 
+slash_loo = svmsmn_loo(data = log.ret, y0 = 0, 
                        draws = draws[ c( 4:6, 
-                                      8:(T+7),
-                                      (T+8):(nrow(draws)) ), ],
-                    cores = 4
+                                         8:(T+7),
+                                         (T+8):(nrow(draws)) ), ],
+                       cores = 4
 )
 slash_criterium = matrix( c(ts_dic, 
-                         ts_loo$estimates[3,1],
-                         ts_waic$estimates[3,1]), nrow = 1)
-row.names( ts_criterium ) = c('ts')
-colnames( ts_criterium ) = c('dic', 'loo', 'waic')
-ts_criterium
-
+                            ts_loo$estimates[3,1],
+                            ts_waic$estimates[3,1]), nrow = 1)
+row.names( slash_criterium ) = c('slash')
+colnames( slash_criterium ) = c('dic', 'loo', 'waic')
+slash_criterium
