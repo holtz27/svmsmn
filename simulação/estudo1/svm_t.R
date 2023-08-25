acdb98d3a12f82c4705f9a7f3f98773878b85428
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-08-25 15:41:21 -0300

    Update svm_t.R

--- simulação/estudo1/svm_t.R
+++ simulação/estudo1/svm_t.R
@@ -15,7 +15,7 @@ nseeds = n_rep
 set.seed( seed )
 seeds = sample(1:1e6, nseeds)
 resultados = list( )
-
+sumario = NULL
 # data setting
 theta = c(mu = 1.0,
           phi = 0.98, 
@@ -33,74 +33,77 @@ burn = 1e3
 lags = 20
 
 # sampling
-for( it in 1:n_rep ){
-  if( it == 1 ) time = Sys.time()
-  #data
-  data = t_data(mu = theta[1], phi = theta[2], sigma = theta[3],
-                b0 = theta[4], b1 = theta[5], b2 = theta[6],
-                y0 = 0,
-                v = theta[7],
-                T = T,
-                seed = seeds[ it ] )
-  y = data$y
-  
-  cat( paste0('réplica: ', it, '\n' ) )
-  
-  # Sampling
-  #N = 
-  samples = svm_t(N,
-                  L_theta = 20, eps_theta = c( 0.5, 0.5, 0.5 ), 
-                  L_b = 20, eps_b = c( 0.1, 0.1, 0.1 ), 
-                  L_h = 50, eps_h = 0.015,
-                  L_v = 10, eps_v = 0.5, alpha = 0.2, li = 2, ls = 40,
-                  y_T = c(0, y), 
-                  seed = 0 )
-  chain_theta = samples$chain$chain_theta
-  chain_b = samples$chain$chain_b
-  chain_h = samples$chain$chain_h
-  chain_v = samples$chain$chain_v
-  chain_l = samples$chain$chain_l
-  # draws
-  draws = matrix(c( chain_theta[1, ],
-                    chain_theta[2, ],
-                    chain_theta[3, ],
-                    chain_b[1, ],
-                    chain_b[2, ],
-                    chain_b[3, ],
-                    chain_v
-  ), nrow = 7, byrow = TRUE)
-  #draws = rbind( draws, chain_h )
-  #draws = rbind( draws, chain_l )
-  ############################### Convergence analysis
-  ################### Trace plots
-  ### burn
-  #burn = 1e1
-  draws = draws[, -c( 1:burn )]
-  #lags = 2
-  jumps = seq(1, N - burn, by = lags)
-  draws = draws[, jumps ]
-  ################### Numeric Analysis
-  resultados[[ it ]] = num_analisys(draws, 
-                                    names = c( 'mu', 'phi', 'sigma', 'b0', 'b1', 'b2', 'v'),
-                                    digits = 4 )
-  vies[ , it ] = resultados[[ it ]][ , 1] - theta
-  smse[ , it ] = vies[ , it ] ** 2
-  
-  cat( '\r' )
-  
-  if( it == n_rep ) time = Sys.time() - time
+for( alpha in c(0.01, 0.2) ){
+  for( it in 1:n_rep ){
+    if( it == 1 ) time = Sys.time()
+    #data
+    data = t_data(mu = theta[1], phi = theta[2], sigma = theta[3],
+                  b0 = theta[4], b1 = theta[5], b2 = theta[6],
+                  y0 = 0,
+                  v = theta[7],
+                  T = T,
+                  seed = seeds[ it ] )
+    y = data$y
+    
+    cat( paste0('réplica: ', it, '; ', 'alpha: ', alpha,'\n' ) )
+    
+    # Sampling
+    #N = 
+    samples = svm_t(N,
+                    L_theta = 20, eps_theta = c( 0.5, 0.5, 0.5 ), 
+                    L_b = 20, eps_b = c( 0.1, 0.1, 0.1 ), 
+                    L_h = 50, eps_h = 0.015,
+                    L_v = 10, eps_v = 0.5, alpha = alpha, li = 2, ls = 40,
+                    y_T = c(0, y), 
+                    seed = 0 )
+    chain_theta = samples$chain$chain_theta
+    chain_b = samples$chain$chain_b
+    chain_h = samples$chain$chain_h
+    chain_v = samples$chain$chain_v
+    chain_l = samples$chain$chain_l
+    # draws
+    draws = matrix(c( chain_theta[1, ],
+                      chain_theta[2, ],
+                      chain_theta[3, ],
+                      chain_b[1, ],
+                      chain_b[2, ],
+                      chain_b[3, ],
+                      chain_v
+    ), nrow = 7, byrow = TRUE)
+    ############################### Convergence analysis
+    ################### Trace plots
+    ### burn
+    #burn = 1e1
+    draws = draws[, -c( 1:burn )]
+    #lags = 2
+    jumps = seq(1, N - burn, by = lags)
+    draws = draws[, jumps ]
+    ################### Numeric Analysis
+    resultados[[ it ]] = num_analisys(draws, 
+                                      names = c( 'mu', 'phi', 'sigma', 'b0', 'b1', 'b2', 'v'),
+                                      digits = 4 )
+    vies[ , it ] = resultados[[ it ]][ , 1] - theta
+    smse[ , it ] = vies[ , it ] ** 2
+    
+    cat( '\r' )
+    
+    if( it == n_rep ){
+      # metrics calculation
+      Vies = apply( vies, MARGIN = 1, mean )
+      Smse = apply( smse, MARGIN = 1, mean )
+      Smse = sqrt( Smse )
+      # summary
+      data = matrix( c(Vies, Smse), ncol = 2 )
+      data = cbind( data , rep(1, 7))
+      row.names( data ) = names = c( 'mu', 'phi', 'sigma', 'b0', 'b1', 'b2', 'v')
+      colnames( data ) = c( 'vies', 'smse', 'alpha' )
+      sumario = cbind( sumario, data )
+      time = Sys.time() - time
+    } 
+  }
 }
 
+
 time 
-
-# metrics calculation
-Vies = apply( vies, MARGIN = 1, mean )
-Smse = apply( smse, MARGIN = 1, mean )
-Smse = sqrt( Smse )
-
-# summary
-sumario = matrix( c(Vies, Smse), ncol = 2 )
-row.names( sumario ) = names = c( 'mu', 'phi', 'sigma', 'b0', 'b1', 'b2', 'v')
-colnames( sumario ) = c( 'vies', 'smse' )
 round( sumario, 4 )
 
