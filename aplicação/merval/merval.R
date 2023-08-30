e9ff38d927ee973b24df739277a21ca65b7bfaec
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-08-30 18:33:10 -0300

    Create merval.R

---
+++ aplicação/merval/merval.R
@@ -0,0 +1,45 @@
+# Reading data
+#ibovespa = read.csv('https://raw.githubusercontent.com/holtz27/rbras/main/aplica%C3%A7%C3%B5es/%5EBVSP.csv')
+merval = read.csv('svm_smn/Aplicação/merval/Merval.csv')
+merval = merval[, c('Data', 'Último')]
+merval[, 2] = stringr::str_remove(merval[, 2], "[.]")
+merval[, 2] = stringr::str_replace(merval[, 2], "[,]", ".")
+merval[, 2] = as.numeric( merval[, 2] ) 
+#merval = na.omit(merval)
+#View(merval)
+T = nrow(merval)
+log.ret = 100 * ( log( merval[2:T, 2] ) - log( merval[1:(T-1), 2] ) )
+merval[, 1] = stringr::str_replace_all(merval[, 1], "[.]", "-")
+dates = as.Date( merval[, 1], "%d-%m-%Y" )
+
+# Plots
+library(ggplot2)
+df = data.frame( Retorno = log.ret, Tempo = dates[-1] )
+
+g = ggplot(df) + geom_line(aes(x = Tempo, y = Retorno))
+g = g + scale_x_date(date_breaks = "18 month", date_labels = "%b %Y")
+g = g + theme_test() + theme(axis.title.y = element_text(size = 18),
+                             axis.text.x = element_text(size = 16),
+                             axis.text.y = element_text(size = 18))
+g = g + xlab('')
+h = ggplot( df, aes(Retorno) )
+h = h + geom_histogram(aes(y = after_stat(density)), bins = 40, color = 'white')
+h = h + theme_test() + ylab('')
+h = h + theme_test() + theme(axis.title.x = element_text(size = 18),
+                             axis.text.x = element_text(size = 18),
+                             axis.text.y = element_text(size = 18))
+gridExtra::grid.arrange(g, h, nrow = 1, ncol = 2) 
+
+data_summary = matrix(c( mean( log.ret ),
+                         sd( log.ret ),
+                         min( log.ret ),
+                         max( log.ret ),
+                         moments::skewness( log.ret ),
+                         moments::kurtosis( log.ret ) ), nrow = 1)
+colnames( data_summary ) = c( 'mean', 'sd', 'min', 'max', 'skewness', 'kurtosis')
+round( data_summary, digits = 2 )
+
+Box.test(log.ret, lag = 12, type = 'Ljung-Box')
+Box.test(log.ret**2, lag = 12, type = 'Ljung-Box')
+acf(log.ret, lag.max = 5, plot = FALSE)
+
