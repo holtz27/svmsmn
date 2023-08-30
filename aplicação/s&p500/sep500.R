eba5a2369586e1fd8cca551ffd9a72707af81b95
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-08-30 18:36:01 -0300

    Create sep500.R

---
+++ aplicação/s&p500/sep500.R
@@ -0,0 +1,46 @@
+#Banco coletado em: 
+#https://br.investing.com/indices/
+# getwd()
+sep500 = read.csv('svm_smn/Aplicação/S&P500/S&P500.csv')
+sep500 = sep500[, c('Data', 'Último')]
+sep500[, 2] = stringr::str_remove(sep500[, 2], "[.]")
+sep500[, 2] = stringr::str_replace(sep500[, 2], "[,]", ".")
+sep500[, 2] = as.numeric( sep500[, 2] ) 
+#View(sep500)
+T = nrow( sep500 )
+log.ret = 100 * ( log( sep500[2:T, 2] ) - log( sep500[1:(T-1), 2] ) )
+sep500[, 1] = stringr::str_replace_all(sep500[, 1], "[.]", "-")
+dates = as.Date( sep500[, 1], "%d-%m-%Y" )
+
+# Plots
+library(ggplot2)
+df = data.frame( Retorno = log.ret , Tempo = dates[-1]  ) #Tempo = dates[-1]
+
+g = ggplot(df) + geom_line(aes(x = Tempo, y = Retorno))
+g = g + scale_x_date(date_breaks = "18 month", date_labels = "%b %Y")
+g = g + theme_test() + theme(axis.title.y = element_text(size = 18),
+                             axis.text.x = element_text(size = 16),
+                             axis.text.y = element_text(size = 18))
+g = g + xlab('')
+h = ggplot( df, aes(Retorno) )
+h = h + geom_histogram(aes(y = after_stat(density)), bins = 40, color = 'white')
+h = h + theme_test() + ylab('')
+h = h + theme_test() + theme(axis.title.x = element_text(size = 18),
+                             axis.text.x = element_text(size = 18),
+                             axis.text.y = element_text(size = 18))
+gridExtra::grid.arrange(g, h, nrow = 1, ncol = 2) 
+
+data_summary = matrix(c( mean( log.ret ),
+                         sd( log.ret ),
+                         min( log.ret ),
+                         max( log.ret ),
+                         moments::skewness( log.ret ),
+                         moments::kurtosis( log.ret ) ), nrow = 1)
+colnames( data_summary ) = c( 'mean', 'sd', 'min', 'max', 'skewness', 'kurtosis')
+round( data_summary, digits = 2 )
+
+Box.test(log.ret, lag = 12, type = 'Ljung-Box')
+Box.test(log.ret**2, lag = 12, type = 'Ljung-Box')
+acf(log.ret, lag.max = 5, plot = FALSE)
+
+
