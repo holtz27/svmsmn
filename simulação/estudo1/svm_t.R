eb033ec02e052f09b3c0217a929b740dfd093b63
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-09-08 11:16:56 -0300

    Update svm_t.R

--- simulação/estudo1/svm_t.R
+++ simulação/estudo1/svm_t.R
@@ -36,18 +36,19 @@ lags = 1
 # sampling
 for( it in 1:n_rep ){
   if( it == 1 ) time = Sys.time()
-  #data
-  data = t_data(mu = theta[1], phi = theta[2], sigma = theta[3],
-                b0 = theta[4], b1 = theta[5], b2 = theta[6],
-                y0 = 0,
-                v = theta[7],
-                T = T,
-                seed = seeds[ it ] )
-  y = data$y
+  
   
   cat( paste0('réplica: ', it, '\n' ) )
   
   repeat{
+    #data
+    data = t_data(mu = theta[1], phi = theta[2], sigma = theta[3],
+                  b0 = theta[4], b1 = theta[5], b2 = theta[6],
+                  y0 = 0,
+                  v = theta[7],
+                  T = T,
+                  seed = seeds[ it ] )
+    y = data$y
     # Sampling
     samples = svm_t(N,
                     L_theta = 20, eps_theta = c( 0.5, 0.5, 0.5 ), 
@@ -76,7 +77,6 @@ for( it in 1:n_rep ){
     #lags
     jumps = seq(1, N - burn, by = lags)
     draws = draws[, jumps ]
-    
     x = apply(draws, MARGIN = 1, FUN = mean)
     if( sum( x ) == Inf ){
       seeds[ it ] = sample(1:1e6, 1)
