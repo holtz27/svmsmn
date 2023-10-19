b34a5d6ae2d65707603bb21c2715c6e8e76e2481
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-10-19 14:51:08 -0300

    Update ibovespa_t.R

--- aplicação/ibovespa/ibovespa_t.R
+++ aplicação/ibovespa/ibovespa_t.R
@@ -3,10 +3,12 @@ ibovespa = read.csv('https://raw.githubusercontent.com/holtz27/rbras/main/aplica
 ibovespa = ibovespa[, c('Date', 'Close')]
 ibovespa[, 2] = as.numeric( ibovespa[, 2] ) 
 ibovespa = na.omit(ibovespa)
-ibovespa = tail(ibovespa, n = 3992)
-#View(ibovespa)
+#ibovespa = tail(ibovespa, n = 1996) #n = 3992
+ibovespa = ibovespa[1:3500,]
+View(ibovespa)
 T = nrow(ibovespa)
-log.ret = 100 * ( log( ibovespa[2:T, 2] ) - log( ibovespa[1:(T-1), 2] ) ) 
+log.ret = 100 * ( log( ibovespa[2:T, 2] ) - log( ibovespa[1:(T-1), 2] ) )
+dates = as.Date( ibovespa[, 1] )
 ################################################################################
 #### librarys
 library( loo )
@@ -19,22 +21,17 @@ path = 'svm_smn/Simulacao/Estudos_Simulacao/ts/svm_t.cpp'
 Rcpp::sourceCpp( path )
 
 #### Sampling
-# alpha
-alpha = .005
-
-N = 5e3
+N = 1e3
 samples = svm_t(N,
                 L_theta = 20, eps_theta = c( 0.5, 0.5, 0.5 ), 
                 L_b = 20, eps_b = c( 0.1, 0.1, 0.1 ), 
                 L_h = 50, eps_h = 0.015,
-                L_v = 5, eps_v = 0.3, alpha = alpha, li = 2, ls = 40,
+                L_v = 20, eps_v = 0.1, alpha = 0.1, li = 2, ls = 40,
                 y_T = c(0, log.ret), 
-                seed = 67092 )
-samples$time / 60
-samples$acc / N
-################## Save outputs
-#save(samples, file = '.RDara')
-#load('.RDara')
+                seed = 0 )
+#samples$time / 60
+#samples$acc / N
+################## Outputs
 chain_theta = samples$chain$chain_theta
 chain_b = samples$chain$chain_b
 chain_h = samples$chain$chain_h
@@ -54,16 +51,25 @@ draws = rbind( draws, chain_l )
 ############################### Convergence analysis
 ################### Trace plots
 ### burn
-burn = 1e3
-draws = draws[, -c( 1:burn )]
-lags = 10
-jumps = seq(1, N - burn, by = lags)
-draws = draws[, jumps ]
+burn = 0
+lags = 20
 # plots
-trace_plots(draws[1:7, ], 
+trace_plots(draws[1:7, ],
+            burn = burn, lags = lags,
             names = c('mu', 'phi', 'sigma', 'b0', 'b1', 'b2', 'v') )
+abs_plots( chain_h, 
+           burn = burn, lags = lags, 
+           date = NULL, 
+           log.ret )
+
+tail_plot( chain_l, 
+           date = NULL,
+           burn = burn, lags = lags,
+           model_name = 't'
+)
 ################### Numeric Analysis
-num_analisys(draws[1:7, ], 
+num_analisys(draws[1:7, ],
+             burn = burn, lags = lags,
              names = c( 'mu', 'phi', 'sigma', 'b0', 'b1', 'b2', 'v'),
              digits = 4 )
 ################################################################################
@@ -73,19 +79,19 @@ num_analisys(draws[1:7, ],
 # param_hat = ( b = b_hat, h = h_hat, l = l_hat )
 ################################################################################
 ############### dic
-ts_dic = svmsmn_dic(data = y, y0 = 0, 
+ts_dic = svmsmn_dic(data = log.ret, y0 = 0, 
                     param_draws = draws[ c( 4:6, 
                                             8:(T+7),
                                             (T+8):(nrow(draws)) ), ]
 )
 ############### waic
-ts_waic = svmsmn_waic(data = y, y0 = 0,
+ts_waic = svmsmn_waic(data = log.ret, y0 = 0,
                       draws = draws[ c( 4:6, 
                                         8:(T+7),
                                         (T+8):(nrow(draws)) ), ]
 )
 ############### loo
-ts_loo = svmsmn_loo(data = y, y0 = 0, 
+ts_loo = svmsmn_loo(data = log.ret, y0 = 0, 
                     draws = draws[ c( 4:6, 
                                       8:(T+7),
                                       (T+8):(nrow(draws)) ), ],
@@ -98,116 +104,5 @@ row.names( ts_criterium ) = c('ts')
 colnames( ts_criterium ) = c('dic', 'loo', 'waic')
 ts_criterium
 
-###############################################################################
-###############################################################################
-############################### l
-l_hat = apply(chain_l, MARGIN = 1, FUN = mean)
-#l_min = apply(chain_l, MARGIN = 1, FUN = quantile, probs = c(0.025) )
-#l_max = apply(chain_l, MARGIN = 1, FUN = quantile, probs = c(0.975) )
-#data2 = matrix(c(1:T, l_hat, l_min, l_max), ncol = 5)
-#data2 = data.frame(data2)
-#names(data2) = c('obs', 'media', 'min', 'max')
-# plot1
-#a = sample(1:(T - 101), 1)
-#f = ggplot(data2[a:(a + 100), ])
-#f = ggplot(data2)
-#f = f + geom_line(aes(obs, media))
-#f = f + geom_line(aes(obs, min), linetype = 'dashed')
-#f = f + geom_line(aes(obs, max), linetype = 'dashed')
-#f
-############### Numeric Analysis
-mcmcchain_l = coda::as.mcmc( t( chain_l ) ) 
-####### Geweke Statistic
-# |G| > 1.96 evidencia não convergencia
-CD_l = coda::geweke.diag( mcmcchain_l )
-# Fração de valores que est]ao no intervalo ( -1.96 , 1.96 )
-geweke_l = sum( abs( CD_l$z ) < 1.96 ) / 2e3
-geweke_l
-####### Fator de ineficiência (IF)
-# IF = N / N_eff, onde N_eff = effective Sample Size
-# Se IF >> 1, indica uma má mistura da cadeia gerada
-N_eff_l = coda::effectiveSize( mcmcchain_l )
-IF_l = N_new / N_eff_l
-mean(IF_l)
-####### MCMC error
-# MCerror = sd( Variavel ) / sqrt( N_eff )
-mc_error_l = round( apply( chain_l, 
-                           MARGIN = 1, 
-                           FUN = sd) / sqrt( N_eff_l ), 5 )
-mean(mc_error_l)
-# plots
-mat = matrix(c(1,1,1,2,3,4), nrow = 2, byrow = TRUE)
-layout( mat )
-plot(l_hat, type = 'l', xlab = 'Tempo', ylab = '')
-plot( CD_l$z, main = 'Geweke diagnostic', xlab = '', ylab = '' )
-abline(h = -1.96, lty = 2, lwd = 5, col = 'blue')
-abline(h = 1.96, lty = 2, lwd = 5, col = 'blue')
-plot( IF_l, main = 'Inefficiency factors', xlab = '', ylab = '' )
-plot( mc_error_l, main = 'MC errors', xlab = '', ylab = '' )
-###############################################################################
-###############################################################################
-############################### h
-h_hat = apply(chain_h, MARGIN = 1, FUN = mean)
-h_min = apply(chain_h, MARGIN = 1, FUN = quantile, probs = c(0.025) )
-h_max = apply(chain_h, MARGIN = 1, FUN = quantile, probs = c(0.975) )
-data = matrix(c(1:T, h, h_hat, h_min, h_max), ncol = 5)
-#data = matrix(c(1:T, h_hat, h_min, h_max), ncol = 4)
-data = data.frame(data)
-names(data) = c('obs', 'vdd', 'media', 'min','max')
-#names(data) = c('obs', 'media', 'min','max')
-#plots
-g1 = ggplot(data[ 1:1000, ])
-g1 = g1 + geom_line(aes(obs, media))
-g1 = g1 + geom_line(aes(obs, vdd), color = 'red')
-g1 = g1 + geom_line(aes(obs, min), linetype = 'dashed')
-g1 = g1 + geom_line(aes(obs, max), linetype = 'dashed')
-g1 = g1 + theme_test()
-g2 = ggplot(data[ 1001:2000, ])
-g2 = g2 + geom_line(aes(obs, media))
-g2 = g2 + geom_line(aes(obs, vdd), color = 'red')
-g2 = g2 + geom_line(aes(obs, min), linetype = 'dashed')
-g2 = g2 + geom_line(aes(obs, max), linetype = 'dashed')
-g2 = g2 + theme_test()
-g1 / g2
-############### Numeric Analysis
-mcmcchain_h = coda::as.mcmc( t( chain_h ) ) 
-####### Geweke Statistic
-# |G| > 1.96 evidencia não convergencia
-CD_h = coda::geweke.diag( mcmcchain_h )
-# Fração de valores que est]ao no intervalo ( -1.96 , 1.96 )
-geweke_h = sum( abs( CD_h$z ) < 1.96 ) / T
-geweke_h
-####### Fator de ineficiência (IF)
-# IF = N / N_eff, onde N_eff = effective Sample Size
-# Se IF >> 1, indica uma má mistura da cadeia gerada
-N_eff_h = coda::effectiveSize( mcmcchain_h )
-IF_h = N_new / N_eff_h
-####### MCMC error
-# MCerror = sd( Variavel ) / sqrt( N_eff )
-mc_error_h = round( apply( chain_h, 
-                           MARGIN = 1, 
-                           FUN = sd) / sqrt( N_eff_h ), 
-                    5 )
-# other plots
-e.vol_hat = apply( exp( chain_h ), MARGIN = 1, FUN = mean )
-e.vol_min = apply( exp( chain_h ), MARGIN = 1, FUN = quantile, probs = c(0.025) )
-e.vol_max = apply( exp( chain_h ), MARGIN = 1, FUN = quantile, probs = c(0.975) )
-data = matrix(c(1:T, abs(y), e.vol_hat, e.vol_min, e.vol_max), ncol = 5)
-data = data.frame(data)
-names(data) = c('obs', 'y.abs', 'e.hat', 'e.min','e.max')
-h = ggplot(data)
-h = h + geom_line(aes(obs, y.abs), color = 'grey70')
-h = h + geom_ribbon(aes(x = 1:T, ymax = e.vol_max, ymin = e.vol_min), 
-                    fill = 'blue' ,alpha = 0.2)
-h = h + geom_line(aes(obs, e.hat), linewidth = 0.75)
-h = h + theme_test() + xlab('Tempo') + ylab('|retornos|')
-h
-# convergence plots
-par( mfrow = c(1,3) )
-plot( CD_h$z, main = 'Geweke diagnostic', xlab = '', ylab = '' )
-abline(h = -1.96)
-abline(h = 1.96)
-plot( IF_h, main = 'Inefficiency factors', xlab = '', ylab = '' )
-abline(h = 1)
-plot( mc_error_h, main = 'MCMC errors', xlab = '', ylab = '' )
-par( mfrow = c(1,1) )
+################## Save
+#save(samples, file = '.RDara')
