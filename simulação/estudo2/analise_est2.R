b0c53819c2a249776150ced17274ec06d664f77f
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-11-06 15:01:02 -0300

    Add files via upload

--- simulação/estudo2/analise_est2.R
+++ simulação/estudo2/analise_est2.R
@@ -1,15 +1,41 @@
 
-x1 = x2 = 0
+load("~/Documentos/svm_smn/svm_t/estudo2/es2_t.RData")
+#load("~/Documentos/svm_smn/svm_vg/estudo2/es2_vg.RData")
 
-for( t in 1:10 ){
+x1 = x2 = 0
+T = length( Data$hmc_times )
+
+for( t in 1:T ){
   x1 = x1 + Data$hmc_eff[, t] / Data$hmc_times[ t ]
   x2 = x2 + Data$rmhmc_eff[, t] / Data$rmhmc_times[ t ]
 }
 
-mean( x1 )
-mean( x2 )
+x1 = x1 / t
+x2 = x2 / t
 
-par( mfrow = c(1, 2) )
-boxplot( x1 )
-boxplot( x2 )
+data = matrix( c(x1, x2), ncol = 2 )
+data = data.frame( data )
+row.names( data ) = c( 'mu', 'phi', 'sigma', 'b0', 'b1', 'b2', 'v' )
+colnames( data ) = c( 'hmc', 'rmhmc' )
+data = round( data, 3 )
+data
+
+
+par( mfrow = c(2, 7) )
+# hmc
+boxplot( as.numeric( Data$hmc_eff[1, ] / Data$hmc_times ) )
+boxplot( as.numeric( Data$hmc_eff[2, ] / Data$hmc_times ) )
+boxplot( as.numeric( Data$hmc_eff[3, ] / Data$hmc_times ) )
+boxplot( as.numeric( Data$hmc_eff[4, ] / Data$hmc_times ) )
+boxplot( as.numeric( Data$hmc_eff[5, ] / Data$hmc_times ) )
+boxplot( as.numeric( Data$hmc_eff[6, ] / Data$hmc_times ) )
+boxplot( as.numeric( Data$hmc_eff[7, ] / Data$hmc_times ) )
+# rmhmc
+boxplot( as.numeric( Data$rmhmc_eff[1, ] / Data$rmhmc_times ) )
+boxplot( as.numeric( Data$rmhmc_eff[2, ] / Data$rmhmc_times ) )
+boxplot( as.numeric( Data$rmhmc_eff[3, ] / Data$rmhmc_times ) )
+boxplot( as.numeric( Data$rmhmc_eff[4, ] / Data$rmhmc_times ) )
+boxplot( as.numeric( Data$rmhmc_eff[5, ] / Data$rmhmc_times ) )
+boxplot( as.numeric( Data$rmhmc_eff[6, ] / Data$rmhmc_times ) )
+boxplot( as.numeric( Data$rmhmc_eff[7, ] / Data$rmhmc_times ) )
 par( mfrow = c(1, 1) )
