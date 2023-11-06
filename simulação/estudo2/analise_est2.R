d8382e23a6469b9527e36533fb00221b2054a4c2
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-11-06 16:48:48 -0300

    Update analise_est2.R

--- simulação/estudo2/analise_est2.R
+++ simulação/estudo2/analise_est2.R
@@ -23,24 +23,39 @@ data
 # boxplot
 par( mfrow = c(1, 7) )
 # hmc
-boxplot( as.numeric( Data$hmc_eff[1, ] / Data$hmc_times[ 1 ] ), xlab = expression(mu), ylab = 'eff/s' )
-boxplot( as.numeric( Data$hmc_eff[2, ] / Data$hmc_times[ 2 ] ), xlab = expression(phi) )
-boxplot( as.numeric( Data$hmc_eff[3, ] / Data$hmc_times[ 3 ] ), xlab = expression(sigma) )
-boxplot( as.numeric( Data$hmc_eff[4, ] / Data$hmc_times[ 4 ] ), xlab = expression(b[0]) )
-boxplot( as.numeric( Data$hmc_eff[5, ] / Data$hmc_times[ 5 ] ), xlab = expression(b[1]) )
-boxplot( as.numeric( Data$hmc_eff[6, ] / Data$hmc_times[ 6 ] ), xlab = expression(b[2]) )
-boxplot( as.numeric( Data$hmc_eff[7, ] / Data$hmc_times[ 7 ] ), xlab = expression(nu) )
+boxplot( as.numeric( Data$hmc_eff[1, ] / Data$hmc_times[ 1 ] ), 
+         xlab = expression(mu), 
+         ylab = 'eff/s', cex.axis = 1.3, cex.lab = 1.5 )
+boxplot( as.numeric( Data$hmc_eff[2, ] / Data$hmc_times[ 2 ] ), xlab = expression(phi),
+         ylab = '', cex.axis = 1.3, cex.lab = 1.5 )
+boxplot( as.numeric( Data$hmc_eff[3, ] / Data$hmc_times[ 3 ] ), xlab = expression(sigma),
+         ylab = '', cex.axis = 1.3, cex.lab = 1.5 )
+boxplot( as.numeric( Data$hmc_eff[4, ] / Data$hmc_times[ 4 ] ), xlab = expression(b[0]),
+         ylab = '', cex.axis = 1.3, cex.lab = 1.5 )
+boxplot( as.numeric( Data$hmc_eff[5, ] / Data$hmc_times[ 5 ] ), xlab = expression(b[1]),
+         ylab = '', cex.axis = 1.3, cex.lab = 1.5 )
+boxplot( as.numeric( Data$hmc_eff[6, ] / Data$hmc_times[ 6 ] ), xlab = expression(b[2]), 
+         ylab = '', cex.axis = 1.3, cex.lab = 1.5 )
+boxplot( as.numeric( Data$hmc_eff[7, ] / Data$hmc_times[ 7 ] ), xlab = expression(nu),
+         ylab = '', cex.axis = 1.3, cex.lab = 1.5 )
 mtext( 'HMC', line = -2, outer = TRUE )
 par( mfrow = c(1, 1) )
 
 par( mfrow = c(1, 7) )
 # rmhmc
-boxplot( as.numeric( Data$rmhmc_eff[1, ] / Data$rmhmc_times[ 1 ] ), xlab = expression(mu), ylab = 'eff/s' )
-boxplot( as.numeric( Data$rmhmc_eff[2, ] / Data$rmhmc_times[ 2 ] ), xlab = expression(phi) )
-boxplot( as.numeric( Data$rmhmc_eff[3, ] / Data$rmhmc_times[ 3 ] ), xlab = expression(sigma) )
-boxplot( as.numeric( Data$rmhmc_eff[4, ] / Data$rmhmc_times[ 4 ] ), xlab = expression(b[0]) )
-boxplot( as.numeric( Data$rmhmc_eff[5, ] / Data$rmhmc_times[ 5 ] ), xlab = expression(b[1]) )
-boxplot( as.numeric( Data$rmhmc_eff[6, ] / Data$rmhmc_times[ 6 ] ), xlab = expression(b[2]) )
-boxplot( as.numeric( Data$rmhmc_eff[7, ] / Data$rmhmc_times[ 7 ] ), xlab = expression(nu) )
+boxplot( as.numeric( Data$rmhmc_eff[1, ] / Data$rmhmc_times[ 1 ] ), xlab = expression(mu), 
+         ylab = 'eff/s', cex.axis = 1.3, cex.lab = 1.5 )
+boxplot( as.numeric( Data$rmhmc_eff[2, ] / Data$rmhmc_times[ 2 ] ), xlab = expression(phi),
+         ylab = '', cex.axis = 1.3, cex.lab = 1.5 )
+boxplot( as.numeric( Data$rmhmc_eff[3, ] / Data$rmhmc_times[ 3 ] ), xlab = expression(sigma),
+         ylab = '', cex.axis = 1.3, cex.lab = 1.5 )
+boxplot( as.numeric( Data$rmhmc_eff[4, ] / Data$rmhmc_times[ 4 ] ), xlab = expression(b[0]),
+         ylab = '', cex.axis = 1.3, cex.lab = 1.5 )
+boxplot( as.numeric( Data$rmhmc_eff[5, ] / Data$rmhmc_times[ 5 ] ), xlab = expression(b[1]),
+         ylab = '', cex.axis = 1.3, cex.lab = 1.5 )
+boxplot( as.numeric( Data$rmhmc_eff[6, ] / Data$rmhmc_times[ 6 ] ), xlab = expression(b[2]),
+         ylab = '', cex.axis = 1.3, cex.lab = 1.5 )
+boxplot( as.numeric( Data$rmhmc_eff[7, ] / Data$rmhmc_times[ 7 ] ), xlab = expression(nu),
+         ylab = '', cex.axis = 1.3, cex.lab = 1.5 )
 mtext( 'RMHMC', line = -2, outer = TRUE )
 par( mfrow = c(1, 1) )
