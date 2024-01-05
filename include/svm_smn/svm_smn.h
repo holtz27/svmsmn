6a164d7d28eac5a8d6b1b47a41a0cb40bcbf4f3c
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-01-05 15:45:09 -0300

    Update svm_smn.h

--- include/svm_smn/svm_smn.h
+++ include/svm_smn/svm_smn.h
@@ -1,5 +1,5 @@
-#ifndef SVM_SMN_TS_H_INCLUDED
-#define SVM_SMN_TS_H_INCLUDED
+#ifndef SVM_SMN_H_INCLUDED
+#define SVM_SMN_H_INCLUDED
 
 #include <RcppArmadillo.h>
 using namespace Rcpp;
@@ -19,8 +19,19 @@ int G_theta(vec theta, mat &G, mat &inv_G, mat &dG_mu, mat &dG_omega, mat &dG_ga
 vec nuH_theta(mat dG_mu, mat dG_omega, mat dG_gamma, mat inv_G, vec p);
 //vec gradHmom_theta(mat inv_G, vec p);
 // hmc updates
-vec hmc_theta(vec theta_cur, vec h, int L, double eps, int T, int &acc);
-vec rmhmc_theta(vec theta_cur, vec h, int fixp, int L, vec eps, int T, int &acc);
+vec hmc_theta(vec theta_cur, vec h, int L, vec eps, int T, int &acc);
+vec rmhmc_theta(vec theta_cur, vec h, int fixp, int L, double eps, int T, int &acc);
+
+// mu_star
+double logpost_mu(double mu, vec theta1,vec h, int T);
+double glogpost_mu(double mu, vec theta1, vec h, int T);
+double hmc_mu(double mu_cur, vec theta1, vec h, int L, double eps, int T, int &acc);
+// theta1
+double logpost_theta1(vec theta1, double mu, vec h, int T);
+vec glogpost_theta1(vec theta1, double mu, vec h, mat inv_G, mat dG_omega, mat dG_gamma, int T);
+int G_theta1(vec theta1, mat &G, mat &inv_G, mat &dG_omega, mat &dG_gamma, int T);
+vec nuH_theta1( mat dG_omega, mat dG_gamma, mat inv_G, vec p );
+vec rmhmc_theta1(vec theta1_cur, double mu, vec h, int fixp, int L, double eps, int T, int &acc);
 
 // beta
 // Model functions
@@ -32,28 +43,45 @@ int G_b(vec b, vec h, vec l, mat &G, mat &inv_G, mat& dG_b0, mat &dG_delta, mat 
 vec nuH_b(mat dG_b0, mat dG_delta, mat dG_b2, mat inv_G, vec p);
 //vec gradHmom_b(mat inv_G, vec p);
 // hmc update
-vec rmhmc_b(vec b_cur, vec h, vec l, int fixp, int L, vec eps, int T, vec y_T , int &acc);
-
-// v
-// Model functions
-double logpost_v(double e, vec l, int T);
-double glogpost_v(double e, vec l, int T);
-// Metrics
-int G_v(vec e, double &G, double &inv_G, double &dG_v, int T);
-// Energy functions
-double nuH_v(double dG_v, double inv_G, double p);
-double gradHmom_v(double inv_G, double p);
-// hmc update
-double rmhmc_v(double e_cur, vec l, int fixp, int L, double eps, int T, int &acc);
-
-// l
-vec l_gibbs(double e, vec y_T, vec h, vec b, int T);
+vec rmhmc_b(vec b_cur, vec h, vec l, int fixp, int L, double eps, int T, vec y_T , int &acc);
 
 // h
 // Model functions
-double logpost_h(vec h, vec theta, vec l, vec b, int T, vec y_T);
-vec glogpost_v(vec e, vec l, mat inv_G, mat dG_v, int T);
+double logpost_h(vec h, vec theta, vec b, vec l, int T, vec y_T);
+vec glogpost_h(vec h, vec theta, vec b, vec l, int T, vec y_T);
 // hmc update
 vec hmc_h(vec h, vec theta, vec b, vec l, int L, double eps, int T, vec y_T, int &acc);
 
-#endif // SVM_SMN_TS_H_INCLUDED
+// -------------------------- SVM - t
+
+// v
+// Model functions
+double logpost_v_t(double e, vec l, int T, double alpha, double li, double ls);
+double glogpost_v_t(double e, vec l, double inv_G, double dG_v, int T, double alpha, double li, double ls);
+// Metrics
+int G_v_t(vec e, double &G, double &inv_G, double &dG_v, int T, double alpha, double li, double ls);
+// Energy functions
+double nuH_v_t(double dG_v, double inv_G, double p);
+double gradHmom_v_t(double inv_G, double p);
+// hmc update
+double rmhmc_v_t(double e_cur, vec l, int fixp, int L, double eps, int T, int &acc, double alpha, double li, double ls );
+
+// l
+vec l_gibbs_t(double e, vec y_T, vec h, vec b, int T, double alpha, double li, double ls);
+
+// -------------------------- SVM - VG
+
+// v
+// Model functions
+double logpost_v_vg(double e, vec l, int T, double alpha, double li, double ls);
+double glogpost_v_vg(double e, vec l, double inv_G, double dG_v, int T, double alpha, double li, double ls);
+// Metrics
+int G_v_vg(vec e, double &G, double &inv_G, double &dG_v, int T, double alpha, double li, double ls);
+// Energy functions
+double nuH_v_vg(double dG_v, double inv_G, double p);
+double gradHmom_v_vg(double inv_G, double p);
+// hmc update
+double rmhmc_v_vg(double e_cur, vec l, int fixp, int L, double eps, int T, int &acc, double alpha, double li, double ls );
+
+
+#endif // SVM_SMN_H_INCLUDED
