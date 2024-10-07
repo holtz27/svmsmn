b40699c1b704dcad1a10a6d783bacb7d988eec2f
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-10-07 14:35:09 -0300

    Update num_analisys.R

--- source/num_analisys.R
+++ source/num_analisys.R
@@ -1,5 +1,6 @@
-num_analisys = function( draws, burn = 0, lags = 1, names, digits ){
+num_analisys = function( draws, burn = 0, lags = 1, names, digits, hdp = FALSE ){
   if( !require(coda) ) install.packages("coda")
+  
   ############### Numeric Analysis
   Draws = draws
   N = ncol( Draws ) 
@@ -14,25 +15,34 @@ num_analisys = function( draws, burn = 0, lags = 1, names, digits ){
   mc_error = apply( chain, 
                     MARGIN = 2, 
                     FUN = sd) / sqrt( N_eff )
-  
   theta_hat = apply( chain, MARGIN = 2, FUN = mean )
   theta_sd  = apply( chain, MARGIN = 2, FUN = sd )
-  theta_min = apply( chain, MARGIN = 2, FUN = quantile, probs = c(0.025) )
-  theta_median = apply( chain, MARGIN = 2, FUN = quantile, probs = c(0.5) )
-  theta_max = apply( chain, MARGIN = 2, FUN = quantile, probs = c(0.975) )
+  
+  if( hdp ){
+    i = coda::HPDinterval( chain )
+    theta_min = i[, 1]
+    theta_max = i[, 2]
+  }else{
+    theta_min = apply( chain, MARGIN = 2, FUN = quantile, probs = c(0.025) )
+    theta_max = apply( chain, MARGIN = 2, FUN = quantile, probs = c(0.975) )
+  }
+  
   data = matrix(
     c(theta_hat,
       theta_sd,
       theta_min,
-      theta_median,
       theta_max,
       CD$z,
       IF,
       N_eff,
       mc_error), nrow = nrow( Draws ), byrow = FALSE
-  )
+    )
   row.names( data ) = names
-  colnames( data ) = c( 'media', 'sd', '2.5%', '50%','97.5%', 'CD', 'IF', 'n_eff', 'MC erro')
+  if( hdp ){
+    colnames( data ) = c( 'media', 'sd', 'HPD.min', 'HPD.max', 'CD', 'IF', 'n_eff', 'MC erro')
+  }else{
+    colnames( data ) = c( 'media', 'sd', '2.5%', '97.5%', 'CD', 'IF', 'n_eff', 'MC erro')
+  }
   
   return( round( data, digits ) )
 }
